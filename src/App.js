2349254b8e197f8b6a25aca6dfc7e8426d019f18
Anke <anke@Ankes-iMac.local>
2021-06-16 17:34:10 +0200

    Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { Subreddits } from './features/subreddits/Subreddits';
 import './App.css';
 
 function App() {
@@ -8,49 +9,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <h2>Hier kommt die Suche hin</h2>
+        <Counter /> 
       </header>
+      <main className="App-mainsection">
+        <p>Hier kommen die Cards mit den Posts und den Kommentaren</p>
+        
+      </main>
+      <aside className="App-aside">
+        <p>Daneben die Filter aka Subreddits</p>
+        <Subreddits />
+      </aside>
+
     </div>
   );
 }
---
+++ src/features/subreddits/Subreddits.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Subreddits.css';
+
+export function Subreddits (){
+
+    return (
+        <div className="Subreddits">
+        <h3>Filter</h3>
+        <ul>
+            <li>Reddit-Books</li>
+            <li>Reddit-Rancoons</li>
+            <li>Reddit-Bikes</li>   
+        </ul>
+        </div>
+    );
+}
+
